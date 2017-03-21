06099701c76438e5e46f2653445331318f63d4ee
Justin <Justin@JUSTINSCOMP>
2017-03-21 12:19:59 -0700

    Add project files.

---
+++ Login/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim EmailLabel As System.Windows.Forms.Label
+        Dim PasswordLabel As System.Windows.Forms.Label
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPW = New System.Windows.Forms.TextBox()
+        Me.MembersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Library_FINAL_FINALDataSet = New Login.Library_FINAL_FINALDataSet()
+        Me.MembersTableAdapter = New Login.Library_FINAL_FINALDataSetTableAdapters.MembersTableAdapter()
+        Me.TableAdapterManager = New Login.Library_FINAL_FINALDataSetTableAdapters.TableAdapterManager()
+        Me.EmployeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.EmployeesTableAdapter = New Login.Library_FINAL_FINALDataSetTableAdapters.EmployeesTableAdapter()
+        Me.Label1 = New System.Windows.Forms.Label()
+        EmailLabel = New System.Windows.Forms.Label()
+        PasswordLabel = New System.Windows.Forms.Label()
+        CType(Me.MembersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Library_FINAL_FINALDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'EmailLabel
+        '
+        EmailLabel.AutoSize = True
+        EmailLabel.Location = New System.Drawing.Point(157, 97)
+        EmailLabel.Name = "EmailLabel"
+        EmailLabel.Size = New System.Drawing.Size(35, 13)
+        EmailLabel.TabIndex = 15
+        EmailLabel.Text = "Email:"
+        '
+        'PasswordLabel
+        '
+        PasswordLabel.AutoSize = True
+        PasswordLabel.Location = New System.Drawing.Point(136, 123)
+        PasswordLabel.Name = "PasswordLabel"
+        PasswordLabel.Size = New System.Drawing.Size(56, 13)
+        PasswordLabel.TabIndex = 17
+        PasswordLabel.Text = "Password:"
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Location = New System.Drawing.Point(214, 168)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLogin.TabIndex = 19
+        Me.BtnLogin.Text = "Login"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(204, 97)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 20
+        '
+        'txtPW
+        '
+        Me.txtPW.Location = New System.Drawing.Point(204, 123)
+        Me.txtPW.Name = "txtPW"
+        Me.txtPW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPW.Size = New System.Drawing.Size(100, 20)
+        Me.txtPW.TabIndex = 21
+        '
+        'MembersBindingSource
+        '
+        Me.MembersBindingSource.DataMember = "Members"
+        Me.MembersBindingSource.DataSource = Me.Library_FINAL_FINALDataSet
+        '
+        'Library_FINAL_FINALDataSet
+        '
+        Me.Library_FINAL_FINALDataSet.DataSetName = "Library_FINAL_FINALDataSet"
+        Me.Library_FINAL_FINALDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'MembersTableAdapter
+        '
+        Me.MembersTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CheckoutTableAdapter = Nothing
+        Me.TableAdapterManager.EmployeesTableAdapter = Nothing
+        Me.TableAdapterManager.MembersTableAdapter = Me.MembersTableAdapter
+        Me.TableAdapterManager.ResourcesTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Login.Library_FINAL_FINALDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'EmployeesBindingSource
+        '
+        Me.EmployeesBindingSource.DataMember = "Employees"
+        Me.EmployeesBindingSource.DataSource = Me.Library_FINAL_FINALDataSet
+        '
+        'EmployeesTableAdapter
+        '
+        Me.EmployeesTableAdapter.ClearBeforeFill = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(44, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(401, 37)
+        Me.Label1.TabIndex = 22
+        Me.Label1.Text = "Welcome to the E-Library"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(491, 226)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPW)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Controls.Add(EmailLabel)
+        Me.Controls.Add(PasswordLabel)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.MembersBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Library_FINAL_FINALDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Library_FINAL_FINALDataSet As Library_FINAL_FINALDataSet
+    Friend WithEvents MembersBindingSource As BindingSource
+    Friend WithEvents MembersTableAdapter As Library_FINAL_FINALDataSetTableAdapters.MembersTableAdapter
+    Friend WithEvents TableAdapterManager As Library_FINAL_FINALDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents BtnLogin As Button
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPW As TextBox
+    Friend WithEvents EmployeesBindingSource As BindingSource
+    Friend WithEvents EmployeesTableAdapter As Library_FINAL_FINALDataSetTableAdapters.EmployeesTableAdapter
+    Friend WithEvents Label1 As Label
+End Class
